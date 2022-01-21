17cb537c9e93ad076e5eaa60dd2748427fa3c734
David Pallmann <dpallmann@hotmail.com>
2022-01-21 16:00:23 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using Amazon;
+using Amazon.Comprehend;
+using Amazon.Comprehend.Model;
+using System.Text;
+
+var region = RegionEndpoint.USWest2;
+
+if (args.Length == 1)
+{
+    var filename = args[0];
+    var analysisType = (args.Length > 1) ? args[1] : "text";
+
+    var client = new AmazonComprehendClient(region);
+
+    Console.WriteLine($"Processing file {filename} with Amazon Comprehend");
+
+    var text = File.ReadAllText(filename);
+
+    var request = new DetectSentimentRequest()
+    {
+        Text = text,
+        LanguageCode = LanguageCode.En
+    };
+
+    Console.WriteLine("--- input text:");
+    Console.WriteLine(text);
+    Console.WriteLine();
+
+    // sentiment analysis
+
+    var response = await client.DetectSentimentAsync(request);
+
+    Console.WriteLine($"Sentiment: {response.Sentiment} (Positive: {response.SentimentScore.Positive:N}% | Negative: {response.SentimentScore.Negative:N}% | Neutral: {response.SentimentScore.Neutral:N}% | Mixed: {response.SentimentScore.Mixed:N}%)");
+
+    var requestEntities = new DetectEntitiesRequest()
+    {
+        Text = File.ReadAllText(filename),
+        LanguageCode = LanguageCode.En
+    };
+
+    // detect entities
+
+    var responseEntities = await client.DetectEntitiesAsync(requestEntities);
+
+    foreach(var entity in responseEntities.Entities)
+    {
+        Console.WriteLine($"entity Type: {entity.Type.Value} | Text: {entity.Text}");
+    }
+
+    // detect personally identifiable information (PII)
+
+    var requestPII = new DetectPiiEntitiesRequest()
+    {
+        Text = File.ReadAllText(filename),
+        LanguageCode = LanguageCode.En
+    };
+
+    var responsePII = await client.DetectPiiEntitiesAsync(requestPII);
+
+    if (responsePII.Entities.Count > 0)
+    {
+        var redactedText = new StringBuilder(requestPII.Text);
+
+        foreach (var entity in responsePII.Entities)
+        {
+            Console.WriteLine($"PII entity Type: {entity.Type.Value} | Text: {entity.BeginOffset}-{entity.EndOffset}");
+            for (var pos = entity.BeginOffset; pos < entity.EndOffset; pos++)
+            {
+                redactedText[pos] = 'X';
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine($"--- redacted text:");
+        Console.WriteLine(redactedText);
+    }
+
+    Environment.Exit(0);
+}
+
+Console.WriteLine("?Invalid parameter - command line format: dotnet run -- <file>");
---
+++ README.md
@@ -0,0 +1,14 @@
+# Hello, Comprehend!
+
+This is the code project for the [Hello, Comprehend!](https://davidpallmann.hashnode.dev/hello-comprehend) blog post. 
+
+This episode: Amazon Comprehend. In this Hello, Cloud blog series, we're covering the basics of AWS cloud services for newcomers who are .NET developers. If you love C# but are new to AWS, or to this particular service, this should give you a jumpstart.
+
+In this post we'll introduce Amazon Comprehend and use it in a "Hello, Cloud" .NET program to perform Natural Language Processing. We'll do this step-by-step, making no assumptions other than familiarity with C# and Visual Studio. We're using Visual Studio 2022 and .NET 6.
+
+## Our Hello, Comprehend Project
+
+After getting familiar with Amazon Comprehend in the AWS Console, you'll write a .NET 6 program that invokes Comprehend to analyze text to 1) analyze sentiment, 2) detect entities, and 3) detect PII and redact the personal information from the text.
+
+See the blog post for the tutorial to create this project and run it on AWS.
+
